08bd8e09d8c8b4c917361372d13713ff596b4aa3
The Yolo Zulu <88694885+kommanderinkief@users.noreply.github.com>
2023-03-14 21:25:38 -0700

    many updates
    - 6hr pool check
    - formatting and grammar fixes
    - Arb and Op working

--- Arb.js
+++ Arb.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 
-//const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 // Replace YOUR_TELEGRAM_BOT_TOKEN with your own Telegram bot token
-//const bot = new TelegramBot('YOUR_TELEGRAM_BOT_TOKEN');
+const bot = new TelegramBot('6253065698:AAFv5-l2kuFLwQtazhLhdN-_zFrfYkxnYbk');
 
 
 async function getPools(pageNO) {
@@ -24,28 +24,32 @@ function checkPool(pool) {
   const tradingVolume5 = pool.attributes.historical_data.last_5m.volume_in_usd;
   const tradingVolume5m = Number(tradingVolume5.replace('$', ''));
 
-  const tradingVolume24h = pool.attributes.historical_data.last_24h.volume_in_usd;
-  const tradingVolume24hm = Number(tradingVolume24h.replace('$', ''));
-  const last_24h_cal = liquidity * 5;
+  const tradingVolume6h = pool.attributes.historical_data.last_6h.volume_in_usd;
+  const tradingVolume6hm = Number(tradingVolume6h.replace('$', ''));
+  //we only want high liquidity so we need to look for liquidty that is higher than actual by using a multiplier?
+  const last_6h_cal = liquidity * 2.5;
 
+
+//figure out exactly what this does!
 if(liquidity < 800000 && liquidity > 100 && tradingVolume > 80 && tradingVolume < 300000){
   if (tradingVolume > liquidity) {
-    const message = `1 Hour: ${pool.attributes.address} volume : $${tradingVolume} liquidity : $${liquidity}. pair name $${pool.attributes.name}  Retio: ${tradingVolume / liquidity}!\n`;
+    const message = `1 Hour: ${pool.attributes.address} Volume : $${tradingVolume} and Liquidity : $${liquidity}. pair name $${pool.attributes.name}  Ratio: ${tradingVolume / liquidity}!\n`;
           console.log(message);
-     //     bot.sendMessage('YOUR_TELEGRAM_CHAT_ID', message);;
+         bot.sendMessage('310580895', message);;
   }
-  
-  if (tradingVolume5m > last_5_min_cal) {
-    const message = `5 min: ${pool.attributes.address} volume of $${tradingVolume5m} liquidity of $${liquidity}. pair name $${pool.attributes.name}  Retio: ${tradingVolume / liquidity}!\n`;
-    console.log(message);
-  //  bot.sendMessage('YOUR_TELEGRAM_CHAT_ID', message);
-  }
-  
 
-  if (tradingVolume24hm > last_24h_cal) {
-    const message = `Last 1 day Pool: ${pool.attributes.address} has trading volume of $${tradingVolume24hm} and liquidity of $${last_24h_cal}  pair name $${pool.attributes.name}  Retio: ${tradingVolume24hm / liquidity}!\n`;
+//check last 5 minss using 5_min_cal which checks is trade volume is 0.13 times greater than liquidity  
+  if (tradingVolume5m > last_5_min_cal) {
+    const message = `5 min: ${pool.attributes.address} Volume of $${tradingVolume5m} and Liquidity of $${liquidity}. pair name $${pool.attributes.name}  Ratio: ${tradingVolume / liquidity}!\n`;
     console.log(message);
-  //  bot.sendMessage('YOUR_TELEGRAM_CHAT_ID', message);
+    bot.sendMessage('310580895', message);
+  }
+  
+//check last 6 hours using 6h_cal which checks is trade volume is 2.5 times greater than liquidity
+  if (tradingVolume6hm > last_6h_cal) {
+    const message = `Last 6 hrs Pools: ${pool.attributes.address} has trading Volume of $${tradingVolume6hm} and Liquidity of $${liquidity}  pair name $${pool.attributes.name}  Ratio: ${tradingVolume6hm / liquidity}!\n`;
+    console.log(message);
+    bot.sendMessage('310580895', message);
   }
 }
 }
